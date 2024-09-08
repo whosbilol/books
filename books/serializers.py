3a9fa3d77624f8ded0f949e5b820ee22090d967a
whosbilol <156183836+whosbilol@users.noreply.github.com>
2024-09-09 00:58:30 +0500

    done

---
+++ books/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import *
+
+class AuthorSerializar(serializers.ModelSerializer):
+    class Meta:
+        model = AuthorModel
+        fields = '__all__'
+
+class CategoriesSerializar(serializers.ModelSerializer):
+    class Meta:
+        model = CategoriesModel
+        fields = '__all__'
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializar()
+    category = CategoriesSerializar()
+
+    class Meta:
+        model = BookModel
+        fields = '__all__'
